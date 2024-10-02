c797d1238d2df859b71d6362b61c1349c6c14eb9
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2024-10-02 17:12:46 -0500

    clears console

--- my-app/components/FullBleedSection.jsx
+++ my-app/components/FullBleedSection.jsx
@@ -5,10 +5,10 @@ import { COLORS } from "../constants";
 const StyledFullBleedSection = styled.div`
   width: 100%;
   padding: 20px 0;
-  background: ${({ bgcolor }) =>
-    bgcolor === "gray"
+  background: ${({ $bgcolor }) =>
+    $bgcolor === "gray"
       ? COLORS.gray
-      : bgcolor === "blueBlend"
+      : $bgcolor === "blueBlend"
       ? COLORS.blueBlend
       : COLORS.white};
 `;
@@ -16,26 +16,26 @@ const StyledFullBleedSection = styled.div`
 const ContentWrapper = styled.div`
   max-width: 1100px;
   margin: 0 auto;
-  background: ${({ bgcolor }) =>
-    bgcolor === "gray"
+  background: ${({ $bgcolor }) =>
+    $bgcolor === "gray"
       ? COLORS.gray
-      : bgcolor === "blueBlend"
+      : $bgcolor === "blueBlend"
       ? COLORS.blueBlend
       : COLORS.white};
 `;
 
 const TextContainer = styled.div`
-  color: ${({ bgcolor }) =>
-    bgcolor === "gray" || bgcolor === "blueBlend"
+  color: ${({ $bgcolor }) =>
+    $bgcolor === "gray" || $bgcolor === "blueBlend"
       ? COLORS.white
       : COLORS.black};
 `;
 
 export default function FullBleedSection({ bgcolor = "white", children }) {
   return (
-    <StyledFullBleedSection bgcolor={bgcolor}>
-      <ContentWrapper bgcolor={bgcolor}>
-        <TextContainer bgcolor={bgcolor}>{children}</TextContainer>
+    <StyledFullBleedSection $bgcolor={bgcolor}>
+      <ContentWrapper $bgcolor={bgcolor}>
+        <TextContainer $bgcolor={bgcolor}>{children}</TextContainer>
       </ContentWrapper>
     </StyledFullBleedSection>
   );
